643e903dec7cc1e369b8ca07df0cabae0d6673c2
utopianism <oopsuto@gmail.com>
2018-01-06 09:04:56 +0800

    rebuild

--- _site/assets/js/lunr-en.js
+++ _site/assets/js/lunr-en.js
@@ -5,29 +5,50 @@ var idx = lunr(function () {
   this.field('tags')
   this.ref('id')
 
-  this.pipeline.remove(lunr.trimmer)
-
-  for (var item in store) {
-    this.add({
-      title: store[item].title,
-      excerpt: store[item].excerpt,
-      categories: store[item].categories,
-      tags: store[item].tags,
-      id: item
-    })
-  }
+  
+  
+    
+    
+      this.add({
+          title: "如何在 Atom 和 WebStorm 上配置 React Native 的代码编辑环境",
+          excerpt: "针对 React Native 的项目，这里主要介绍什么是 Eslint 和 Flow ，以及在 Atom 和 WebStorm 上如何配置它们。 Eslint Eslint 作为静态代码检查工具，在团队代码中可以起到统一代码风格的作用，同时会适当降低代码编写过程中出现的一些非必要性错误。 ESLint 官方网站上的介绍： JavaScript 是一个动态的弱类型语言，在开发中比较容易出错。因为没有编译程序，为了寻找 JavaScript...",
+          categories: ["React-Native"],
+          tags: ["React-Native","React","javascript"],
+          id: 0
+      })
+      
+    
+  
 });
 
 console.log( jQuery.type(idx) );
 
+var store = [
+  
+    
+    
+    
+      
+      {
+        "title": "如何在 Atom 和 WebStorm 上配置 React Native 的代码编辑环境",
+        "url": "http://localhost:4000/react-native/2018/01/02/config-react-native-at-Atom-and-WebStorm.html",
+        "excerpt": "针对 React Native 的项目，这里主要介绍什么是 Eslint 和 Flow ，以及在 Atom 和 WebStorm 上如何配置它们。 Eslint Eslint 作为静态代码检查工具，在团队代码中可以起到统一代码风格的作用，同时会适当降低代码编写过程中出现的一些非必要性错误。 ESLint 官方网站上的介绍： JavaScript 是一个动态的弱类型语言，在开发中比较容易出错。因为没有编译程序，为了寻找 JavaScript...",
+        "teaser":
+          
+            null
+          
+      }
+    
+  ]
+
 $(document).ready(function() {
   $('input#search').on('keyup', function () {
     var resultdiv = $('#results');
-    var query = $(this).val().toLowerCase();
+    var query = $(this).val().toLowerCase().replace(":", "");
     var result =
       idx.query(function (q) {
         query.split(lunr.tokenizer.separator).forEach(function (term) {
-          q.term(term, { boost: 100 })
+          q.term(term, {  boost: 100 })
           if(query.lastIndexOf(" ") != query.length-1){
             q.term(term, {  usePipeline: false, wildcard: lunr.Query.wildcard.TRAILING, boost: 10 })
           }
@@ -50,7 +71,7 @@ $(document).ready(function() {
               '<div class="archive__item-teaser">'+
                 '<img src="'+store[ref].teaser+'" alt="">'+
               '</div>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
             '</article>'+
           '</div>';
       }
@@ -61,7 +82,7 @@ $(document).ready(function() {
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
               '</h2>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
             '</article>'+
           '</div>';
       }
